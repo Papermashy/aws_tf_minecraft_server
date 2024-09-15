2fd1ee760e4c86d7fe8dd5250ca351693910a9a2
rory <rory@rorypringle.com>
2024-09-15 14:49:09 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_ami" "minecraft_server_ami" {
+  
+}
+
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
